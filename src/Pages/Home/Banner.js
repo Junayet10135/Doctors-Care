3a1e792f6ec0186354f0ca92bfdcb7925db6fc3b
JunayetTamim <junayettamim186@gmail.com>
2022-05-16 01:24:09 +0600

    Footer update

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -5,14 +5,14 @@ import PrimaryButton from '../Shared/PrimaryButton';
 
 const Banner = () => {
     return (
-        <div class="hero min-h-screen " style={{
+        <div className="hero min-h-screen " style={{
             backgroundImage: `url(${bg})`
         }}>
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} class="max-w-lg rounded-lg shadow-2xl" />
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} className="max-w-lg rounded-lg shadow-2xl" />
                 <div>
-                    <h1 class="text-5xl font-bold">Your New Smile Starts Here!</h1>
-                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <h1 className="text-5xl font-bold">Your New Smile Starts Here!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <PrimaryButton>Get Started</PrimaryButton>
                 </div>
             </div>
--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -30,7 +30,7 @@ const Footer = () => {
                 </div>
             </div>
             <div className='my-10 text-center'>
-                <p>Copyright © 2022 - All right reserved</p>
+                <p>Copyright © 2022 - All right reserved By JH Tamim</p>
             </div>
         </footer>
     );
