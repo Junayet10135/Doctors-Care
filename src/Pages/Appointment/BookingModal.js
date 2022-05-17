c983dc1868178b4c52ce3f508ddf53f9b787c6ee
JunayetTamim <junayettamim186@gmail.com>
2022-05-17 16:36:07 +0600

    warning issue solve

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,8 +1,11 @@
 import { format } from 'date-fns';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const BookingModal = ({ date, treatment, setTreatment }) => {
     const { _id, name, slots } = treatment;
+    const [user] = useAuthState(auth);
 
     const handleBooking = event => {
         event.preventDefault();
@@ -23,11 +26,13 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
                         <input type="text" disabled value={format(date, 'PP')} class="input input-bordered w-full max-w-xs" />
                         <select name="slot" class="select select-bordered w-full max-w-xs">
                             {
-                                slots.map(slot => <option value={slot}>{slot}</option>)
+                                slots.map((slot, index) => <option 
+                                key={index}
+                                value={slot}>{slot}</option>)
                             }
                         </select>
-                        <input type="text" name="name" placeholder="Your Name" class="input input-bordered w-full max-w-xs" />
-                        <input type="email" name="email" placeholder="Email Address" class="input input-bordered w-full max-w-xs" />
+                        <input type="text" name="name" disabled value={user?.displayName || ''} class="input input-bordered w-full max-w-xs" />
+                        <input type="email" name="email" disabled value={user?.email || ''} class="input input-bordered w-full max-w-xs" />
                         <input type="text" name="phone" placeholder="Phone Number" class="input input-bordered w-full max-w-xs" />
                         <input type="submit" value="Submit" class="btn btn-secondary w-full max-w-xs" />
                     </form>
